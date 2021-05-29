a17ccfadad58fbba5aa16f82f493d8a9b401782c
alrasyidlathif <alrasyidlathif@gmail.com>
2021-05-29 16:35:00 +0700

    add indexes on article

--- models/article.js
+++ models/article.js
@@ -2,29 +2,37 @@ const Sequelize = require('sequelize')
 
 const db = require('../configs/database')
 
-const Article = db.define('articles', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+const Article = db.define('articles',
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        author: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        body: {
+            type: Sequelize.TEXT,
+            allowNull: false
+        },
+        created: {
+            type: Sequelize.DATE,
+            allowNull: false
+        }
     },
-    author: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    title: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    body: {
-        type: Sequelize.TEXT,
-        allowNull: false
-    },
-    created: {
-        type: Sequelize.DATE,
-        allowNull: false
+    {
+        indexes: [
+            {unique: false, fields:['author']},
+            {unique: false, fields:['title']}
+        ]
     }
-})
+)
 
 module.exports = Article
