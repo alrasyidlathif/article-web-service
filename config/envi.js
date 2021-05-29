36735d377b991eb5dfb103cf76f81d6f23bef5ab
alrasyidlathif <alrasyidlathif@gmail.com>
2021-05-29 14:58:07 +0700

    fixing env

--- config/envi.js
+++ config/envi.js
@@ -1,20 +1,23 @@
 const dotenv = require('dotenv')
 const path =  require('path')
 
-dotenv.config({ path: path.join(__dirname, '.env') })
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
-const ENV_VAR = [
-    'DBNAME', 'DBUSER', 'DBPWD'
-]
+const ENV_VAR = ['DBNAME', 'DBUSER', 'DBPWD']
 
 for (let i of ENV_VAR) {
-    if (!process.env[i]) process.exit(1)
+    if (!process.env[i]) {
+        console.log('env missing')
+        process.exit(1)
+    }
+}
+
+const db = {
+    name: process.env.DBNAME,
+    user: process.env.DBUSER,
+    pwd: process.env.DBPWD
 }
 
 module.exports = {
-    db: {
-        name: process.env.DBNAME,
-        user: process.env.DBUSER,
-        pwd: process.env.DBPWD
-    }
+    db
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const db = require('./database')
 const routes = require('./routes')
 
 const app = express()
